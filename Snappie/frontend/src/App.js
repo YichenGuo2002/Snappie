53a5cb9e1db8b3c1a755d5722b15d209ebc6ff1e
Yichen Guo <yg2258@nyu.edu>
2023-05-26 18:16:17 -0400

    logo added.

--- Snappie/frontend/src/App.js
+++ Snappie/frontend/src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
+import logo from './static/logo.png'
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hi, trying tailwind css</p>
+    <div className="App w-full min-h-screen">
+      <div className = "topBar h-32 flex flex-row justify-between pt-8 pb-8 pl-32 pr-32">
+        <img src={logo} className = "h-full object-contain object-center align-middle" alt="logo"/>
+        <p>Hi</p>
+      </div>
+
+      <nav className = "navBar border border-black w-1/4">
+        <p>History</p>
+      </nav>
+
+      <div className = "chat">
+        <p>Hi</p>
+      </div>
+
     </div>
   );
 }
