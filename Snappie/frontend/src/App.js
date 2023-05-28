e7b8e17795b06f4ed77e409f67127a1cf2c6fc31
Yichen Guo <yg2258@nyu.edu>
2023-05-28 11:54:25 -0400

    send function added.

--- Snappie/frontend/src/App.js
+++ Snappie/frontend/src/App.js
@@ -5,6 +5,36 @@ import React from 'react';
 function App() {
   let response1 = "Octavate is a company that positions itself as an audio art dealer brand. Their main focus is on reactivating undervalued talent in the music industry through their unique artist management system. They combine expertise in music, data, and finance to identify and support promising artists who may have been overlooked or underappreciated.";
 
+  let send = async function(){
+    const textarea = document.getElementById('message'); 
+    const chatArea = document.getElementById('chatArea'); 
+    
+    const value = textarea.value.trim();
+    
+    if (value.length >= 1) {
+      console.log(value)
+      const promptQuestion = document.createElement('div');
+      promptQuestion.className = 'promptQuestion';
+      
+      const userParagraph = document.createElement('p');
+      userParagraph.textContent = 'Yichen';
+      
+      const responseParagraph = document.createElement('p');
+      responseParagraph.textContent = value;
+      
+      promptQuestion.appendChild(userParagraph);
+      promptQuestion.appendChild(responseParagraph);
+      
+      chatArea.appendChild(promptQuestion);
+      
+      // Clear the textarea value
+      textarea.value = '';
+      
+      // Scroll to the bottom of the chatArea div
+      chatArea.scrollTop = chatArea.scrollHeight;
+    }
+  }
+
   return (
     <div className="App w-full min-h-screen">
       <div className = "topBar h-24 flex flex-row justify-start pt-8 pb-8 pl-32 pr-32">
@@ -12,12 +42,13 @@ function App() {
       </div>
 
       <div className = "flex flex-row">
-        <nav className = "navBar h-menu w-1/4 flex flex-col items-center p-4 relative border">
-          <button className = "navBut m-2 p-2 text-xl">My Dashboard</button>
-          <button className = "navBut m-2 p-2 text-xl">Snappie Activity</button>
-          <button className = "navBut m-2 p-2 text-xl">FAQ</button>
-          <button className = "navBut m-2 p-2 text-xl">Updates</button>
-          <button className = "navBut m-2 p-2 text-xl">Help</button>
+        <nav className = "navBar h-menu w-1/4 flex flex-col items-center p-4 relative">
+          <button className = "navBut">My Dashboard</button>
+          <button className = "bg-chat text-xl p-2 w-full">Sexual Harrassment Training</button>
+          <button className = "navBut">Snappie Activity</button>
+          <button className = "navBut">FAQ</button>
+          <button className = "navBut">Updates</button>
+          <button className = "navBut">Help</button>
           <div className = "account absolute bottom-8 w-3/4">
             <hr/>
             <p>Account: Yichen</p>
@@ -32,42 +63,24 @@ function App() {
             <p>Progress: 40%</p>
           </div>
 
-          <div className = "chatArea grow overflow-auto">
+          <div className = "chatArea grow overflow-auto" id="chatArea">
             <div className = "promptResponse">
-              <p>Yichen</p>
-              <p>{response1}</p>
-            </div>
-            <div className = "promptQuestion">
-              <p>Yichen</p>
-              <p>{response1}</p>
-            </div>
-            <div className = "promptResponse">
-              <p>Yichen</p>
-              <p>{response1}</p>
-            </div>
-            <div className = "promptQuestion">
-              <p>Yichen</p>
-              <p>{response1}</p>
-            </div>
-            <div className = "promptResponse">
-              <p>Yichen</p>
-              <p>{response1}</p>
-            </div>
-            <div className = "promptQuestion">
               <p>Yichen</p>
               <p>{response1}</p>
             </div>
           </div>
 
-          <form className="prompt  w-full p-4 bg-chat rounded-b-chat">
-            <textarea id="message" rows="1" 
-            className="block resize-none overflow-hidden p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg 
-            border border-black focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-            placeholder="Send a message...">
-            </textarea>
-            <button>Send</button>
+          <div className="prompt  w-full p-4 bg-chat rounded-b-chat">
+            <div className="inline-block w-full flex flex-row">
+              <textarea id="message" rows="1" 
+              className="block resize-none overflow-hidden p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg 
+              border border-black focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+              placeholder="Send a message...">
+              </textarea>
+              <button onClick = {send} className="border border-black rounded-lg">Send</button>
+            </div>
             <p className="">Snappie may display inaccurate or offensive information that doesn’t represent Snap Instruct's views.</p>
-          </form>
+          </div>
         </div>
       </div>
     </div>
